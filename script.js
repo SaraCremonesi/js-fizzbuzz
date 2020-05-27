bd05cc04bc19a72cbf0f3bd210cbb4ffd39bbfac
SaraCremonesi <saracrem00@gmail.com>
2020-05-27 17:55:58 +0200

    correzione

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ var elemento;
 
 // Ciclo for ed if per controllare i multipli di 3, 5 ed entrambi
 for (var i = 0; i <= 100; i++) {
-  if (i % 3 === 0) {
-    elemento = 'Fizz';
+  if ((i % 3 === 0) && (i % 5 === 0)) {
+    elemento = 'FizzBuzz';
   } else if (i % 5 === 0) {
     elemento = 'Buzz';
-  } else if ((i % 3 === 0) && (i % 5 === 0)) {
-    elemento = 'FizzBuzz';
+  } else if (i % 3 === 0) {
+    elemento = 'Fizz';
   } else {
     elemento = i;
   }
 
   // Operazione di stampa a schermo della lista ottenuta
   lista.innerHTML = lista.innerHTML + '<li>' + elemento + '</li>'
+
 };
